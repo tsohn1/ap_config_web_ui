e071b154f2cc73b8f70d9fca29b30dd90a9cbe76
tsohn1 <sohnte@gmail.com>
2023-07-05 11:47:31 +0900

    Implement page that displays YAML data

--- main.go
+++ main.go
@@ -1,20 +1,31 @@
 package main
 
 import (
-	"fmt"
-	"os"
 	"ap_config_web_ui/config"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
 
 )
 
 var network_env config.NetworkEnv
 
 func main() {
+	
 	parsed_network_env, err := config.GetConfigEnv("config_files/network.yaml", &network_env)
 	if err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
-	fmt.Printf("%+v\n", parsed_network_env)
+
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*")
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "index.html", parsed_network_env)
+	})
+
+	router.Run(":8080")
 
 }
