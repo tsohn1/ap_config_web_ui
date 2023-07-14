a395d03d78f72fac1933582ec2417e04487e94e6
tsohn1 <sohnte@gmail.com>
2023-07-14 16:27:29 +0900

    Display operation.yaml content on webpage

--- main.go
+++ main.go
@@ -18,6 +18,7 @@ import (
 const (
 	YAML_FOLDER = "config_files/"
 	NETWORK_ENV = YAML_FOLDER + "network.yaml"
+	OPERATION_ENV = YAML_FOLDER + "operation.yaml"
 	VALIDATE_YAML_CHANGES = true //flag to validate YAML changes using gRPC
 	GRPC_SUCCESS_TOKEN = 1
 	GRPC_FAIL_TOKEN = 0
@@ -25,14 +26,60 @@ const (
 
 var (
 	network_env config.NetworkEnv
+	operation_env config.OperationEnv
 	client validate.ValidateClient
+	operationFieldTypes = map[string]string{
+		"HomeDir" :           "string",
+		"ConfigDir" :         "string",
+		"CertFile" :          "string",
+		"LogDir" :            "string",
+		"TmpDir" :            "string",
+		"TagImageDir" :       "string",
+		"TemplateDir" :       "string",
+		"FontDir" :           "string",
+		"ImageDir" :          "string",
+		"ExternalBinaryDir" : "string",
+		"DataBackupDir" :     "string",
+		"LogLevel" :                  "string",
+		"ApBrokerRetryTimingSecond" : "int",
+		"ImgGenThreadCount" :         "int",
+		"ImgGenReqPort" :             "string",
+		"ImgGenRespPort" :            "string",
+		"ImgGenPubPort" :             "string",
+		"EslApTimerReqPort" :         "string",
+		"DeassignThreadCount" :       "int",
+		"FontFacePreloadCount" :      "int",
+		"LastTaskIdBackupFile" :  "string",
+		"ProductDataBackupFile" : "string",
+		"AssignDataBackupFile" :  "string",
+		"NfcDataBackupFile" :     "string",
+		"EventFrameTxTiming" :    "int",
+		"TagImageTxTimging" :     "int",
+		"ScanProfile" :           "[6]int",
+		"BackoffBase" :           "int",
+		"BackoffMulFactor" :      "int",
+		"FreezerTagMultiplier" :  "int",
+		"TagDistributionMinute" : "int",
+		"PageRotationMacPage" :   "int",
+		"LogMaxSizeMb" :  "int",
+		"LogMaxBackup" :  "int",
+		"LogMaxAgeDays" : "int",
+		"LogCompress" :   "bool",
+		"GRpcMaxSize" :   "int",
+	}
 )
+
+type OperationData struct {
+	Data  map[string]interface{}
+	Types map[string]string
+}
 
 func readNetworkConfig(ctx *gin.Context) {
 	//parse YAML from yaml directory
 	parsed_network_env, err := config.GetConfigEnv(NETWORK_ENV, &network_env)
 	if err != nil {
 		log.Println("Error:", err)
+		log.Println("readNetworkConfig")
 		os.Exit(1)
 	}
 
@@ -61,6 +108,7 @@ func updateNetworkConfig(ctx *gin.Context) {
 
 	if err != nil {
 		log.Println("Error:", err)
+		log.Println("updateNetworkConfigAtoi")
 		os.Exit(1)
 	}
 
@@ -89,6 +137,7 @@ func updateNetworkConfig(ctx *gin.Context) {
 
 	if err != nil {
 		log.Println("Error:", err)
+		log.Println("updateNetworkConfig  SetConfigEnv")
 		os.Exit(1)
 	}
 	if VALIDATE_YAML_CHANGES {
@@ -105,11 +154,36 @@ func updateNetworkConfig(ctx *gin.Context) {
 	ctx.Redirect(http.StatusSeeOther, "/network.html")
 }
 
-
 func loadHomePage(ctx *gin.Context) {
 	ctx.HTML(http.StatusOK, "index.html", nil)
 }
 
+func readOperationConfig(ctx *gin.Context) {
+	//parse YAML from yaml directory
+	parsed_operation_env, err := config.GetConfigEnv(OPERATION_ENV, &operation_env)
+	if err != nil {
+		log.Println("Error:", err)
+		log.Println("readOperationConfig")
+		os.Exit(1)
+	}
+
+	//type assertion to operationenv
+	operation_env_struct := parsed_operation_env.(*config.OperationEnv)
+
+	//change interface to map type to pass to html
+	operation_env_map := structs.Map(operation_env_struct)
+	
+	ctx.HTML(http.StatusOK, "operation.html", gin.H{
+		"YAMLData" : OperationData{
+				Data : operation_env_map,
+				Types: operationFieldTypes,
+		},
+	})
+}
+
+func updateOperationConfig(ctx *gin.Context) {
+	return
+}
 func main() {
 
 	if VALIDATE_YAML_CHANGES {
@@ -137,6 +211,11 @@ func main() {
 	// write to network YAML when form is submitted, update webpage as well
 	router.POST("/network.html", updateNetworkConfig)
 
+	// diplay operation YAML data in webpage
+	router.GET("/operation.html", readOperationConfig)
+
+	// write to operation YAML when form is submitted, update webpage as well
+	router.POST("/operation.html", updateOperationConfig)
 	
 	router.Run(":8080")
 
